183df9d49092becff2ff636eb4e7730355d4a2d7
JamesPonderIII <jhponderiii@gmail.com>
2024-09-18 21:47:21 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,41 @@
 // Import libraries here
+import java.util.Random;
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println(x:"JJJJJJJJJJJJJJJJ    PPPPPPPPPPPPPP");
+    System.out.println(x:"JJJJJJJJJJJJJJJJ    PPPPPPPPPPPPPP");
+    System.out.println(x:"      JJJJJ         PPPP       PPP");
+    System.out.println(x:"      JJJJJ         PPPP       PPP");
+    System.out.println(x:"      JJJJJ         PPPPPPPPPPPPPP");
+    System.out.println(x:"      JJJJJ         PPPPPPPPPPPPPP");
+    System.out.println(x:"JJJJ  JJJJJ         PPPP");
+    System.out.println(x:"JJJJ  JJJJJ         PPPP");
+    System.out.println(x:"JJJJJJJJJJJ         PPPP");
+    System.out.println(x:"JJJJJJJJJJJ         PPPP");
+    
+    double a,c;
+             	    Scanner sc=new Scanner(System.in);	   	 
+	    System.out.println("Please enter a number in Fahrenheit:");
+                   a=sc.nextDouble(); 
+	    System.out.println(+celsius(a));		  	  	     
+	} 
+	static double celsius(double f)
+	{	
+	return  (f-32)*5/9;
+
+    System.out.println(x:"Please enter a 5-character string:");
+  String string = sc,nextLine();
+  String reveString = "";
+  return String(1, s.length() - 2);
+
+    Random rand = new Random();
+    int numb = rand.nextInt([32 - 16384])
+
+System.out.println(x:"Random number generated. Continuing...");
+    
+    System.out.println("Your new string is" + +celsius(a)+string+rand);
   }
 }
